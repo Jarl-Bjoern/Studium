d48ecb0e590463b2ae68e381a10bdbe422c614f8
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-31 20:30:27 +0100

    Update Room_Herold_Schwab.py

--- rooms/Room_Herold_Schwab.py
+++ rooms/Room_Herold_Schwab.py
@@ -60,8 +60,11 @@ class Room_Herold_Schwab(EscapeRoom):
     # Level 1
     def RCE(self, URL):
         from requests import get
+        from requests.exceptions import *
 
-        r = get(f'{URL}?cmd=ls')
+        try: r = get(f'{URL}?cmd=ls')
+        except ConnectionError: r = "OK"
+
         return r
 
     # Level 2
