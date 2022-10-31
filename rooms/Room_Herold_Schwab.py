8598d9a9f0807ac2ce10ec815d42adff41e57670
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-31 16:32:39 +0100

    Update Room_Herold_Schwab.py

--- rooms/Room_Herold_Schwab.py
+++ rooms/Room_Herold_Schwab.py
@@ -15,38 +15,34 @@ class Room_Herold_Schwab(EscapeRoom):
 
     ### LEVELS ###
     def create_level1(self):
-        mysterious_letters = self.random_letters()
-
         task_messages = ["Du wachst in einem Raum auf und siehst einen Laptop. Du setzt dich auf den Stuhl und schaust auf den Display.",
             "Auf dem Display steht, dass du 5 Minuten Zeit hast, in der vorgegebenen URL eine moegliche Remote Code Execution anzuhaengen",
-            "http://localhost:8000/index.html"
-            f"Schreibe eine Methode <code>run('{mysterious_letters}')</code>, die aus den Buchstaben den richtigen Code erzeugt."]
+            "http://localhost:8000/index.html",
+            f"Schreibe eine Methode <code>run()</code>, die aus den Buchstaben den richtigen Code erzeugt."]
         hints = ["Es gibt verschiedene Module um Webrequests zu nutzen.",
             "Überlege dir einen Systembefehl aus Linux und versuche ihn sinnvoll in die URL einzubauen."]
 
-        return {"task_messages": task_messages, "hints": hints, "solution_function": self.RCE, "data": mysterious_letters}
+        return {"task_messages": task_messages, "hints": hints, "solution_function": self.RCE}
 
     def create_level2(self):
         task_messages = ["Nach der korrekten Eingabe des Codes hoerst du einen alarmieren Ton, auf dem Display erscheint eine neue Nachricht.",
-                        "Du hast maximal 5 Minuten Zeit, einen Sortier Algorithmus zu folgendem String zu generieren!"
-                        "H134E23589L34563L23333222244L23335555112334O"
-                        "Gebe danach den sortierten String nach folgendem Beispiel aus."
+                        "Du hast maximal 5 Minuten Zeit, einen Sortieralgorithmus zu folgendem String über eine Methode run() zu generieren!",
+                        "String: H134E23589L34563L23333222244L23335555112334O",
+                        "Gebe im Anschluss den sortierten String nach folgendem Beispiel aus.",
                         "1. A","2. B", "3. C"]
         hints = ["Wie kann am effizientesten ein Sortieralgorithmus generiert werden?", 
                 "Probiere es mit einer Hilfsvariable"]
-        return {"task_messages": task_messages, "hints": hints, "solution_function": self.remove_vowels, "data": "Vokale verboten"}
+        return {"task_messages": task_messages, "hints": hints, "solution_function": self.Sort_Chars_In_Text}
 
     def create_level3(self):
-        mysterious_letters = self.random_letters()
-
         task_messages = ["Du hast den zweiten Teil bestanden. Nun bemerkst du, dass auf dem Display ein neuer Dialog erscheint."
             "Auf dem Display steht, dass du nun 10 Minuten Zeit hast, ein Passwort anhand der vorliegenden Dateien zu generieren",
             "Hinter einem an der Wand aufgehängten Bild siehst du nun ein Eingabepanel für einen 6-ziffrigen Code.",
-            f"Schreibe eine Methode <code>run('{mysterious_letters}')</code>, die aus den Buchstaben den richtigen Code erzeugt."]
+            f"Schreibe eine Methode <code>run()</code>, die alle Dateien überprüft und anschließend ein Passwort formt.",]
         hints = ["Es gibt ein bestimmtes Modul in Python, mit dem man auf Betriebssystemebene arbeiten kann.",
             "Versuche die englische Variante des Wortes Betriebssystem."]
 
-        return {"task_messages": task_messages, "hints": hints, "solution_function": self.List_Files_Build_Pass, "data": mysterious_letters}
+        return {"task_messages": task_messages, "hints": hints, "solution_function": self.List_Files_Build_Pass}
 
     def random_letters(self):
         letters = ""
