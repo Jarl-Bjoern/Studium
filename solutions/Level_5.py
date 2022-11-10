f4d4a94a23bff41256f5b174960d2840fe96c489
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-10 23:12:00 +0100

    Create Level_5.py

---
+++ solutions/Level_5.py
@@ -0,0 +1,11 @@
+def run(Path):
+    Temp_Array = []
+    with open(Path, 'r') as f:
+            for line in f.read().splitlines():
+                Datum = rsplit(r':|\s', line)
+
+                if (Datum[3][1:].split('/')[1] == 'Mar' and Datum[3][1:].split('/')[0] == '07' and 'POST' in Datum[8]):
+                    Ausgabe = f'{Datum[0]} {Datum[3][1:]} {Datum[4]}:{Datum[5]}:{Datum[6]} {Datum[7][1:3]}:{Datum[7][3:-1]} {Datum[8]} {Datum[9]} {Datum[10]}'
+                    if (Ausgabe not in Temp_Array):
+                        Temp_Array.append(Ausgabe)
+    return Temp_Array
